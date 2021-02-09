38cab58197e059fb4bef21f7f95331743aa6c9ce
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-09 15:43:05 +0100

    Add test environment validation

--- conftest.py
+++ conftest.py
@@ -5,6 +5,10 @@ from app import db, app
 
 @pytest.fixture
 def client():
+    if app.config['ENV'] != 'testing':
+        raise EnvironmentError(
+            "It's not testing environment! Set correct environment variable."
+        )
     db.create_all()
     yield app.test_client()
     db.drop_all()
