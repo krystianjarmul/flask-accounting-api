80ae5ba78647240e6ee4a0f58d1ca1c471839a64
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-09 22:13:54 +0100

    Add craete job feature

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields, ValidationError, validates
 
 app = Flask(__name__)
 
-ma = Marshmallow(app)
 
 if app.config['ENV'] == 'testing':
     app.config.from_object('config.TestingConfig')
@@ -23,9 +23,16 @@ class Employee(db.Model):
         self.name = name
 
 
-class EmployeeSchema(ma.Schema):
-    class Meta:
-        fields = ['id', 'name']
+class EmployeeSchema(Schema):
+    id = fields.Integer()
+    name = fields.String(required=True)
+
+    @validates('name')
+    def validate_name(self, name):
+        if not name:
+            raise ValidationError('No name was given')
+        elif not isinstance(name, str):
+            raise ValidationError('The given name is not of type string')
 
 
 @app.route('/employees/', methods=['GET'])
@@ -36,3 +43,31 @@ def list_employee():
     result = employees_schema.dump(employees)
 
     return jsonify(result), 200
+
+
+@app.route('/employees/<int:employee_id>')
+def retrieve_employee(employee_id):
+    employee = Employee.query.get(employee_id)
+    employee_schema = EmployeeSchema()
+    if employee is None:
+        return jsonify({'error': 'An employee does not exist.'}), 404
+
+    result = employee_schema.dump(employee)
+
+    return result, 200
+
+
+@app.route('/employees/', methods=['POST'])
+def create_employee():
+    employee = Employee(request.json['name'])
+    employee_schema = EmployeeSchema()
+
+    try:
+        result = employee_schema.load(request.json)
+    except ValidationError as e:
+        return e.messages, 400
+    db.session.add(employee)
+    db.session.commit()
+
+
+    return result, 201
--- test_api_employee.py
+++ test_api_employee.py
@@ -5,6 +5,10 @@ from app import app, Employee, db
 EMPLOYEE_URL = '/employees/'
 
 
+def detail_url(employee_id):
+    return f'{EMPLOYEE_URL}{employee_id}'
+
+
 def add_employee(name):
     employee = Employee(name)
     db.session.add(employee)
@@ -12,13 +16,54 @@ def add_employee(name):
 
 
 def test_retrieve_list_of_employees(client):
-    attrs = {'name': 'Alina Testowska'}
-    add_employee(attrs['name'])
+    add_employee('Alina Testowska')
+    add_employee('Katarzyna Tester')
+    add_employee('Teresa Testarossa')
 
     res = client.get(EMPLOYEE_URL)
     data = res.get_json()
 
     assert res.status_code == 200
-    assert len(data) == 1
-    assert data[0]['id'] == 1
-    assert data[0]['name'] == attrs['name']
+    assert len(data) == 3
+
+
+def test_retrieve_single_employee_successful(client):
+    add_employee('Alina Testowska')
+
+    res = client.get(detail_url(1))
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data['id'] == 1
+    assert data['name'] == 'Alina Testowska'
+
+
+def test_retrieve_single_employee_fails(client):
+    res = client.get(detail_url(2))
+
+    assert res.status_code == 404
+    assert b'An employee does not exist' in res.data
+
+
+def test_create_an_employee_successful(client):
+    payload = {
+        'name': 'Agata Test'
+    }
+
+    res = client.post(EMPLOYEE_URL, json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 201
+    assert data['name'] == 'Agata Test'
+
+
+def test_create_an_employee_with_invalid_payload(client):
+    payload = {
+        'name': ''
+    }
+
+    res = client.post(EMPLOYEE_URL, json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 400
+    assert b'No name was given' in res.data
