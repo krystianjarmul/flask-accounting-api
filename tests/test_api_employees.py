8ec2bf9f07bfc9fd1d29fdecd955ae2d3916957c
Krystian Jarmul <krystian.jarmul@ericsson.com>
2021-02-18 06:55:56 +0100

    Add update with put method to employees API

--- tests/test_api_employees.py
+++ tests/test_api_employees.py
@@ -87,6 +87,20 @@ def test_create_an_employee_with_empty_payload_fails(client):
     assert data['path'] == '/employees'
 
 
+def test_update_an_employee_successfully(client):
+    employee_id = add_employee('Alina Testowa')
+    payload = {
+        'name': 'Maria Test'
+    }
+
+    res = client.put(detail_url(employee_id), json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data['name'] == payload['name']
+    assert data['id'] == employee_id
+
+
 def test_partial_update_an_employee_successfully(client):
     employee_id = add_employee('Alina Testowa')
     payload = {
